2a685cd819055c994d54b4c01a5186cfccc602df
Muhammad Ali Kazmi <m_kazmi497@hotmail.com>
2025-04-23 12:33:22 +0500

    products crud operations completed

--- product/models.py
+++ product/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    sku = models.CharField(max_length=100, unique=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory_quantity = models.PositiveIntegerField()
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
